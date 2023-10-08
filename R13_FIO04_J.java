18ddcc3879285039aeb708e79be6cddaf341d5dc
guythatsepic <jasonbrodie@comcast.net>
2023-10-08 18:35:35 -0400

    Update R13_FIO04_J.java

--- R13_FIO04_J.java
+++ R13_FIO04_J.java
@@ -1,3 +1,6 @@
+// Rule 13. Input Output (FIO)
+// FIO04-J. Release resources when they are no longer needed
+
 try (FileInputStream stream = new FileInputStream(fileName);
      BufferedReader bufRead =
          new BufferedReader(new InputStreamReader(stream))) {
